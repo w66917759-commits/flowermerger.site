759d9d4afa23e7a12e35285535661e05c0d150d5
w66917759-commits <w66917759-commits@users.noreply.github.com>
2026-04-19 22:11:36 +0800

    Export static product site

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10897714
+  "domain": "5gvci.com",
+  "zoneId": 10897714
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts("https://5gvci.com/act/files/service-worker.min.js?r=sw")
